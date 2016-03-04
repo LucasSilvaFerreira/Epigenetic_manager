5c51772ad705b440b0350050b6061085f97a554c
lucas <bioinfolucas@gmail.com>
2016-03-04 15:07:45 -0300

    Several fix and modifications

--- EpiManager/__init__.py
+++ EpiManager/__init__.py
@@ -41,6 +41,34 @@ def open_file_fast(nome_do_arquivo, delimitador='\n', tabulador_interno='none'):
                          '      ;(ponto e virgula) \n      :(dois pontos) \n      n(enter)\n')
         sys.exit(1)
 
+def bed_flank(bed_string, down= 200, up= 200):
+    '''Given a bed_string, return a string with the two flank regions
+    Parameters:
+            bed_string : string bed separated por tab
+                String in bed format
+
+            down/up : int defaut(1000)
+                The count of nucleotides extended in boundaries to create the flanks
+
+
+    returns: str
+        string with the two flanks separated for '\n'
+
+    '''
+
+    br = bed_string.split('\t')
+
+    br[2] =  str(int(br[1])-1)
+    br[1] = str(int(br[1])-down)
+    br='\t'.join(br)
+
+    bl = bed_string.split('\t')
+    bl[1] =  str(int(bl[2])+1)
+    bl[2] = str(int(bl[1])+up)
+    bl='\t'.join(bl)
+
+    return '\n'.join([br, bl])
+
 def bed_start_site_to_tss(bed_string, down = 1000, up = 1000, not_reverse= True):
     '''dado uma coordenada *bed_string* retorna aquela string com o inicio e fim da coordenada sendo a extensão
         do start site dessa sequencia dada
@@ -55,6 +83,10 @@ def bed_start_site_to_tss(bed_string, down = 1000, up = 1000, not_reverse= True)
         '''
     saida_str = ''
     #print bed_string
+
+    print type(bed_string), bed_string
+
+
     if "\t-" in bed_string:
         bed_string_tabulada =  bed_string.split('\t')
         ref_coord = int(bed_string_tabulada[2])
@@ -99,11 +131,29 @@ def bed_get_midle_point(bed_string):
     bed_string : str
         bed_string separated  by tab
     -------------------
-    string with mid point'''
+
+    returns list:
+
+    return [0]:
+    string with mid point and the coords with midpoint +1
+    ex: mid_point 100
+    return [1]:
+
+        ex: mid_point 100
+        chr 100 101
+
+    '''
     bed_string = bed_string.split('\t')
     #print bed_string
-    midpoint = ((int(bed_string[2])-int(bed_string[1]))/2)
-    return str(int(bed_string[1]) + midpoint)
+
+    midpoint = int(bed_string[2])-((int(bed_string[2])-int(bed_string[1]))/2)
+    print 'midpoint', midpoint, bed_string[2], bed_string[1]
+    second_return = bed_string
+    print 'saida antes de processar', second_return
+    second_return[1] = str(midpoint)
+    second_return[2] = str(midpoint+1)
+    print 'saida', '\t'.join(second_return)
+    return str(int(bed_string[1]) + midpoint), '\t'.join(second_return)
 
 def plot_bed_to_closest_bed_mark(bed_gene_file, bed_histone_mark, color='g' , plot_name_save=None, label=None, range_plot=None, show=False):
     '''Given a *bed_gene_file* coord file  and a *bed_histone_mark*, generates a distance density between bedfile and mark midle
@@ -213,7 +263,7 @@ def bam_sort_and_index_alinged_number(bam_file):
 
     return aligned_size, complete_out
 
-def coverage_bins(bam_file, string_bed_bins, name, to_string=False):
+def coverage_bins(bam_file, string_bed_bins, name, adjust_bed_in, to_string=False):
     '''Calculates the coverage given a bam_file and a bedfile
     Parameters:
          bam_file <str>: A bam file
@@ -223,9 +273,40 @@ def coverage_bins(bam_file, string_bed_bins, name, to_string=False):
     '''
     temp_bed_name = '{}_bed_slice_temp.bed'.format(name)
     bed_temp = open(temp_bed_name, 'w')
-    for x_line in open_file_fast(string_bed_bins):
-        bed_temp.write(x_line + '\n')
-    bed_temp.close()
+    ### checking adjust_bed_bin parameters
+
+    print adjust_bed_in
+    if not any([x for x in adjust_bed_in.values()]):
+        for x_line in [l for l in open_file_fast(string_bed_bins) if not re.search('^#', l)]:
+            bed_temp.write(x_line + '\n')
+        bed_temp.close()
+
+    elif len([[f, x]for f, x in adjust_bed_in.iteritems() if x != False]) > 2:
+        sys.stderr.write( 'Error so many args in adjustament parameter' + "\n")
+        print [[f, x] for f, x in adjust_bed_in.iteritems() if x != False]
+        sys.exit()
+
+    elif adjust_bed_in['r'] != False:
+        for x_line in [l for l in open_file_fast(string_bed_bins) if not re.search('^#', l)]:
+            # print '-'*20
+            # print x_line
+            # print bed_get_midle_point(x_line)[1]
+            # print bed_start_site_to_tss(bed_get_midle_point(x_line)[1], down=adjust_bed_in['r'][0], up=adjust_bed_in['r'][1])
+
+            bed_temp.write(bed_start_site_to_tss(bed_get_midle_point(x_line)[1], down=adjust_bed_in['r'][0], up=adjust_bed_in['r'][1]) + '\n')
+        bed_temp.close()
+
+
+    elif adjust_bed_in['f'] != False:
+        for x_line in [l for l in open_file_fast(string_bed_bins) if not re.search('^#', l)]:
+            flanks =  bed_flank(bed_string=x_line,
+                          down=adjust_bed_in['f'][0],
+                          up=adjust_bed_in['f'][1])
+
+            bed_temp.write(flanks+ '\n')
+        bed_temp.close()
+
+
     #print 'bedtools coverage -abam {bam_file} -b temp/bed_slice_temp.bed'.format(bam_file=bam_file)
     os.system('bedtools coverage  -abam {bam_file} -b {bed_slice_temp}> {name_temp}_coverage_temp.bed'.format(bam_file=bam_file, name_temp=name, bed_slice_temp=temp_bed_name ))
     os.system('rm {}'.format(temp_bed_name))
@@ -239,13 +320,20 @@ def coverage_bins(bam_file, string_bed_bins, name, to_string=False):
         #os.system('rm {name}_+sorted_coverage_temp.bed'.format(name=name))
         return '{name}_sorted_coverage_temp.bed'.format(name=name)
 
-def get_normalized_count_in_bed_region_given_a_mark(bam, bed_target, analysis_name):
+def get_normalized_count_in_bed_region_given_a_mark(bam, bed_target, analysis_name, resize_bed=False, flank_bed=False):
     '''Given a bam file and a Bed_file computes the normalized (rpkm) inside bed regions
     Parameters:
         bam <str>: A bam file path
         bed_target <str>: A bed file path
         analysis_name <str> : A string with the unique mark_name
+        resize_bed list<int> default<False>: The original bed will be resized with base in you center
+                                ex: resize_bed = [100,500]
+                                chr   100-start stop+500
 
+        flank_bed list<int> default<False>: The original bed will be transformed in  2 flanks regions
+                                ex: flank_bed = [100,500]
+                                chr   100-start start-1
+                                chr   stop+1    stop+500
     return:
 
         bed_target with the last column containing the normalized count value
@@ -255,13 +343,14 @@ def get_normalized_count_in_bed_region_given_a_mark(bam, bed_target, analysis_na
 
     '''
     factor, sort_bam = bam_sort_and_index_alinged_number(bam)
-    cov = coverage_bins(bam_file=sort_bam, string_bed_bins=bed_target, name=analysis_name)
+
+    cov = coverage_bins(bam_file=sort_bam, string_bed_bins=bed_target, name=analysis_name, adjust_bed_in={'r':resize_bed, 'f':flank_bed})
     norma = __normalization__(cov, factor)
     os.system('rm {}'.format(cov))
 
     return norma
 
-def get_several_normalized_count_in_bed_region_given_a_mark(hash_bam, bed_target):
+def get_several_normalized_count_in_bed_region_given_a_mark(hash_bam, bed_target,  r_bed=False, f_bed=False ):
     '''
     This is a batch version of  function  get_normalized_count_in_bed_region_given_a_mark()
 
@@ -275,9 +364,22 @@ def get_several_normalized_count_in_bed_region_given_a_mark(hash_bam, bed_target
 
 
 
+        r_bed list<int> default<False>: The original bed will be resized with base in you center
+                                ex: r_bed = [100,500]
+                                chr   100-start stop+500
+
+        f_bed list<int> default<False>: The original bed will be transformed in  2 flanks regions
+                                ex: f_bed = [100,500]
+                                chr   100-start start-1
+                                chr   stop+1    stop+500
+
+
+
+
+
     '''
 
-    all_results_dict = {name_analysis:get_normalized_count_in_bed_region_given_a_mark(bam=bam, bed_target=bed_target, analysis_name=name_analysis) for name_analysis, bam in hash_bam.iteritems()}
+    all_results_dict = {name_analysis: get_normalized_count_in_bed_region_given_a_mark(bam=bam, bed_target=bed_target, analysis_name=name_analysis, resize_bed=r_bed, flank_bed=f_bed) for name_analysis, bam in hash_bam.iteritems()}
 
 
 
@@ -290,7 +392,7 @@ def get_several_normalized_count_in_bed_region_given_a_mark(hash_bam, bed_target
     plt.xlim(-5,5)
     plt.show()
 
-def get_groups_normalized_count_in_bed_region_given_a_mark(hash_in_groups_bam, bed_target_hash):
+def get_groups_normalized_count_in_bed_region_given_a_mark(hash_in_groups_bam, bed_target_hash, r_bed=False, f_bed=False ):
     '''
     This is a group batch version of  function  get_normalized_count_in_bed_region_given_a_mark()
 
@@ -320,6 +422,17 @@ def get_groups_normalized_count_in_bed_region_given_a_mark(hash_in_groups_bam, b
                                  }
 
 
+        r_bed list<int> default<False>: The original bed will be resized with base in you center
+                                ex: r_bed = [100,500]
+                                chr   100-start stop+500
+
+        f_bed list<int> default<False>: The original bed will be transformed in  2 flanks regions
+                                ex: f_bed = [100,500]
+                                chr   100-start start-1
+                                chr   stop+1    stop+500
+
+
+
 
     '''
 
@@ -330,7 +443,7 @@ def get_groups_normalized_count_in_bed_region_given_a_mark(hash_in_groups_bam, b
         bed_target = bed_target_hash[bed_name]
         hash_group_bed = bed_file_group
 
-        all_results_dict = {name_analysis:get_normalized_count_in_bed_region_given_a_mark(bam=bam, bed_target=bed_target, analysis_name=name_analysis) for name_analysis, bam in hash_group_bed.iteritems()}
+        all_results_dict = {name_analysis: get_normalized_count_in_bed_region_given_a_mark(bam=bam, bed_target=bed_target, analysis_name=name_analysis, resize_bed=r_bed, flank_bed=f_bed) for name_analysis, bam in hash_group_bed.iteritems()}
 
         df_mix = []
         for name, lista_out in all_results_dict.iteritems():
@@ -348,17 +461,14 @@ def get_groups_normalized_count_in_bed_region_given_a_mark(hash_in_groups_bam, b
 
     concact_df_array = pd.concat(df_array_to_paste)
 
-    print concact_df_array
+    #print concact_df_array
     pd.DataFrame.to_csv(concact_df_array, 'tabela_grupo.txt' , sep='\t', index=False)
     sns.boxplot(x="Group", y="Count", hue="Mark", data=concact_df_array, palette="Set2")
 
 
     #sns.boxplot(data=df_to_box, orient="h", palette="Set2")
-    plt.xlim(-5,5)
+    plt.ylim(-5,5)
     plt.show()
-
-
-
 
 
 def main():
@@ -368,9 +478,14 @@ def main():
     #
 
 
-    # print get_normalized_count_in_bed_region_given_a_mark(bam= '/work/users/lucassilva/chromhmm/sra_dir/peaks_hmm/H3K4me1/H3K4me1.bam',
-    #                                                 bed_target='/work/users/lucassilva/chromhmm/sra_dir/peaks_hmm/AR_24/AR_24.bed',
-    #                                                 analysis_name='H3K4ME1')
+    print len(get_normalized_count_in_bed_region_given_a_mark(bam= '/work/users/lucassilva/chromhmm/sra_dir/peaks_hmm/H3K4me1/H3K4me1.bam',
+                                                    bed_target='/work/users/lucassilva/chromhmm/sra_dir/peaks_hmm/AR_24/AR_24.bed',
+                                                    analysis_name='H3K4ME1', flank_bed=[10, 10]))
+
+    print len(get_normalized_count_in_bed_region_given_a_mark(bam= '/work/users/lucassilva/chromhmm/sra_dir/peaks_hmm/H3K4me1/H3K4me1.bam',
+                                                    bed_target='/work/users/lucassilva/chromhmm/sra_dir/peaks_hmm/AR_24/AR_24.bed',
+                                                    analysis_name='H3K4ME1', resize_bed=[10, 10]))
+
 
 
     # get_several_normalized_count_in_bed_region_given_a_mark(hash_bam = {'H3K4ME1':'/work/users/lucassilva/chromhmm/sra_dir/peaks_hmm/H3K4me1/H3K4me1.bam',
@@ -382,29 +497,29 @@ def main():
     #
 
 
-    bam_groups = {'amostra1':{'H3K4ME1':'/work/users/lucassilva/chromhmm/sra_dir/peaks_hmm/H3K4me1/H3K4me1.bam',
+    bam_groups = {'exon':{'H3K4ME1':'/work/users/lucassilva/chromhmm/sra_dir/peaks_hmm/H3K4me1/H3K4me1.bam',
                               'H3K4ME3':'/work/users/lucassilva/chromhmm/sra_dir/peaks_hmm/H3K4me1/H3K4me1.bam'},
 
 
-                  'amostra2':{'H3K4ME1':'/work/users/lucassilva/chromhmm/sra_dir/peaks_hmm/H3K4me1/H3K4me1.bam',
+                  'tss':{'H3K4ME1':'/work/users/lucassilva/chromhmm/sra_dir/peaks_hmm/H3K4me1/H3K4me1.bam',
                               'H3K4ME3':'/work/users/lucassilva/chromhmm/sra_dir/peaks_hmm/H3K4me1/H3K4me1.bam'}
 
 
 
                   }
 
-    bed_target_hash = {'amostra1':'/work/users/lucassilva/chromhmm/sra_dir/peaks_hmm/AR_24/AR_24.bed',
+    bed_target_hash = {'exon':'/work/users/lucassilva/chromhmm/sra_dir/peaks_hmm/AR_24/AR_24.bed',
 
-                       'amostra2':'/work/users/lucassilva/chromhmm/sra_dir/peaks_hmm/AR_24/AR_24.bed',
+                       'tss':'/work/users/lucassilva/chromhmm/sra_dir/peaks_hmm/AR_24/AR_24.bed',
                        }
 
 
 
 
-    get_groups_normalized_count_in_bed_region_given_a_mark(hash_in_groups_bam =bam_groups , bed_target_hash= bed_target_hash)
+    #get_groups_normalized_count_in_bed_region_given_a_mark(hash_in_groups_bam =bam_groups , bed_target_hash= bed_target_hash)
 
 
-    t#able_df = pd.read_table('/home/lucas/PycharmProjects/Epigenetic_manager/EpiManager/tabela_grupo.txt', delimiter='\t')
+    #table_df = pd.read_table('/home/lucas/PycharmProjects/Epigenetic_manager/EpiManager/tabela_grupo.txt', delimiter='\t')
 
     # print table_df[table_df['Count'].map(type)==pd.]
 
@@ -419,7 +534,7 @@ def main():
     #print table_df.info()
     #sns.boxplot(x="Group", y="Count", hue="Mark", data=table_df, palette="Set2")
 
-    plt.show()
+    #plt.show()
 
 
 
